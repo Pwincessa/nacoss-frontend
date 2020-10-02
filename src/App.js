7c431bc207ce565802a92132bb122929ffed2175
Elozzy <st.eloghosa@gmail.com>
2020-10-02 06:49:17 +0100

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={NavBar} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="top-navigation navbar navbar-expand-lg navbar-light">
+        <Link class="navbar-brand" to="#" style={{ marginLeft: "100px" }}>
+          <h2 id="naccoss_text">
+            NACOSS <br /> <span style={{ color: "#ac0d7f" }}>UNIBEN</span>
+          </h2>
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav nav">
+            <li class="nav-item active spacing">
+              <Link class="nav-link  nav-link-text" activeClassName="current" to="/">
+                HOME
+              </Link>
+            </li>
+            <li class="nav-item spacing ">
+              <Link class="nav-link nav-link-text" activeClassName="current" to="#">
+                ABOUT
+              </Link>
+            </li>
+            <li class="nav-item spacing">
+              <Link class="nav-link nav-link-text" activeClassName="current" to="#">
+                EVENT
+              </Link>
+            </li>
+            <li class="nav-item spacing">
+              <Link class="nav-link nav-link-text" activeClassName="current" to="#">
+                REGISTRATION
+              </Link>
+            </li>
+            <li class="nav-item spacing">
+              <Link class="nav-link nav-link-text" activeClassName="current" to="#">
+                PAYMENT
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
