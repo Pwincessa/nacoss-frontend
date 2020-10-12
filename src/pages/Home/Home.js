8345dfcc9c31f3721dfa85a104fcf08e1cde0609
Elozzy <st.eloghosa@gmail.com>
2020-10-12 16:40:26 +0100

    currently responsive  and cards done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,61 @@
 import React from "react";
 import Header from "@/components/Header/Header";
-import cycle from "../../assets/images/cycle.svg";
+import cycle from "@/assets/images/cycle.svg";
 import "./Home.css";
+import { cardDetails } from "./data";
+import Button from "@/components/Button/Button";
 
 const Home = () => {
   return (
     <>
-    {/* currently not responsive */}
       <Header text={"Welcome to NACOSS UNIBEN. We believe in building a digital future"} />
       <div className="container mt-5">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12">
-            <h2 className="mission-statement bottom-line">
+            <h2 className="mission-statement ">
               Mission and Vision of <br /> NACOSS UNIBEN
             </h2>
+            <hr className="bottom-line" />
             <p className="statement">
               NACOSS organizes capacity building exercises to enlighten the youths, the young people of
               Nigeria on how to become entrepreneurs using acquired IT skills and also to create platforms to
               exhibit their products, services and software to businesses, industry and the public.
             </p>
-            <button className="mission-learn-btn">Learn more</button>
+
+            <button type="button" class="btn btn-primary btn-lg btn-color">
+              Learn More <i class="fa fa-long-arrow-right arrow-right" aria-hidden="true"></i>
+            </button>
           </div>
           <div className="col-lg-6 col-md-6 col-sm-12 ">
-            <img alt="cycle" src={cycle} />
+            <img alt="cycle" class="img-fluid" src={cycle} />
           </div>
+        </div>
+      </div>
+      {/* Event-section */}
+      <div className="container mt-5">
+        <h2 className="d-flex justify-content-center" id="event-header-text">
+          EVENTS
+        </h2>
+        <hr className="short-bottom-line d-flex justify-content-center" />
+        <div className="row">
+          {cardDetails.map((data, index) => {
+            return (
+              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-4 " key={index}>
+                <div className="card">
+                  <img src={data.photo} class="card-img" alt="images" />
+                  <div class="card-img-overlay">
+                    <h6 class="card-text move text-white">{data.date}</h6>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">{data.programDetails}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="d-flex justify-content-center">
+          <Button text="See More" />
         </div>
       </div>
     </>
---
+++ src/pages/Home/data.js
@@ -0,0 +1,17 @@
+export const cardDetails = [
+  {
+    photo: require("../../assets/images/cardimg1.png"),
+    date: "coming soon in July",
+    programDetails: ".NET CONF coming up in July, dates to be released"
+  },
+  {
+    photo: require("../../assets/images/cardimg-2.png"),
+    date: "April 5th,  2020",
+    programDetails: "Tech day and career exhibition"
+  },
+  {
+    photo: require("../../assets/images/cardimg-3.png"),
+    date: "coming soon in July",
+    programDetails: "Business management and product management meetup"
+  }
+]
